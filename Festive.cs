bed406fd167da9bdb841ab58aace6fdbfc7461b0
helado de brownie <helado.de.brownie@gmail.com>
2020-12-25 18:48:11 -0500

    further refine behavior

--- Festive.cs
+++ Festive.cs
@@ -11,6 +11,8 @@ namespace XRL.World.Parts
         public const string GAME_STATE_NAME = "helado_Festive Trees_Festive";
 
         public string Tile = null;
+        public string ColorString = "&G";
+        public string DetailColor = "R";
 
         [GameBasedCacheInit]
         public static void GameBasedCacheInit()
@@ -36,15 +38,46 @@ namespace XRL.World.Parts
             {
                 ParentObject.RemovePart(this);
             }
+
+            base.Attach();
         }
+
+        public override bool WantEvent(int id, int cascade)
+        {
+            return
+                id == GetDisplayNameEvent.ID ||
+                id == GetShortDescriptionEvent.ID ||
+                id == GetShortDisplayNameEvent.ID ||
+            base.WantEvent(id, cascade);
+
+        }
+
+        public override bool HandleEvent(GetShortDescriptionEvent @event)
+        {
+            @event.Postfix.Append("\nThis object has been decorated for some festive purpose by an {{M-K distribution|unknown entity}}.\n");
+            return base.HandleEvent(@event);
+        }
+
+        public override bool HandleEvent(IDisplayNameEvent @event)
+        {
+            @event.AddAdjective("{{G-Y-R-E sequence|festive}}");
+            return base.HandleEvent(@event);
+        }
+
         public override bool Render(RenderEvent @event)
         {
-            if (Tile != null)
+            @event.ColorString = ColorString;
+            @event.DetailColor = DetailColor;
+
+            if (ParentObject.Understood() && Tile != null)
             {
                 @event.Tile = Tile;
+                return true;
             }
-
-            return base.Render(@event);
+            else
+            {
+                return base.Render(@event);
+            }
         }
     }
 }
