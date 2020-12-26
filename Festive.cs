4a16c34f096058572723425a2d89f7bd8d0361ca
helado de brownie <helado.de.brownie@gmail.com>
2020-12-25 19:07:53 -0500

    forgot to remove this when it wasn't what i needed

--- Festive.cs
+++ Festive.cs
@@ -5,7 +5,6 @@ using static XRL.World.Parts.helado_FestiveTrees_Festive;
 
 namespace XRL.World.Parts
 {
-    [HasGameBasedStaticCache]
     public class helado_FestiveTrees_Festive : IPart
     {
         public const string GAME_STATE_NAME = "helado_Festive Trees_Festive";
@@ -13,17 +12,6 @@ namespace XRL.World.Parts
         public string Tile = null;
         public string ColorString = "&G";
         public string DetailColor = "R";
-
-        [GameBasedCacheInit]
-        public static void GameBasedCacheInit()
-        {
-            var Game = XRLCore.Core?.Game;
-
-            if (Game != null && !Game.HasBooleanGameState(GAME_STATE_NAME))
-            {
-                Game.SetBooleanGameState(GAME_STATE_NAME, true);
-            }
-        }
 
         public override void Attach()
         {
